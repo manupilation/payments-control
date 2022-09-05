66fa84aa2820565411a5322b52fecf64c03d2dc9
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-09-05 20:38:15 -0300

    feat: add payments table

---
+++ backend/src/database/migrations/20220904232412-create-payments.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-undef */
+/* eslint-disable camelcase */
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('payments', { 
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      patient: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      procedure: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      paid: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      date: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      total_value: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      },
+      portion_value: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      },
+      qt_portion: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    });
+
+  },
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('payments');
+  }
+};
