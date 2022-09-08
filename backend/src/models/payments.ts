a7875ff988f750e7a3a65d14f67b222a3acfc41a
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-09-08 15:21:44 -0300

    feat: implemet getPayById

--- backend/src/models/payments.ts
+++ backend/src/models/payments.ts
@@ -1,4 +1,14 @@
 import Payment from '../database/models/Payments';
 import IPayment, { RegisterPay } from '../types/interfaces/Payments';
 
-export default class PaymentsModel {}
+export default class PaymentsModel {
+  getPayById = async (id: number) => {
+    const data = await Payment.findOne({
+      where: {
+        id,
+      },
+    });
+
+    return data;
+  };
+}
