5687d1a448541341a0c0818637d35869c8037579
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-09-08 13:57:54 -0300

    feat: init Entry model

--- backend/src/database/models/Entry.ts
+++ backend/src/database/models/Entry.ts
@@ -8,4 +8,32 @@ class Entry extends Model {
   declare procedure: number;
 }
 
+Entry.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+
+  value: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+  },
+
+  procedure: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  }
+}, {
+  sequelize: db,
+  timestamps: false,
+  underscored: true,
+  tableName: 'entries',
+});
+
 export default Entry;
