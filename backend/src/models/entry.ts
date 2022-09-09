3ecbc5b98782a7f6b2778d768f9ffc7a00323bab
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-09-08 23:03:56 -0300

    feat: createEntry Method

--- backend/src/models/entry.ts
+++ backend/src/models/entry.ts
@@ -1,3 +1,12 @@
 import Entry from '../database/models/Entry';
+import { RegisterEntry } from '../types/interfaces/Entry';
 
-export default class EntryModel {}
+export default class EntryModel {
+  createEntry = async (data: RegisterEntry) => {
+    const creating = await Entry.create({
+      data,
+    });
+
+    return creating;
+  };
+}
