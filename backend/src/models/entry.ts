88b8b4e5abd8315f906cb704cbd05e8c345a7791
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-09-09 18:32:31 -0300

    feat: create entry method get all

--- backend/src/models/entry.ts
+++ backend/src/models/entry.ts
@@ -2,6 +2,12 @@ import Entry from '../database/models/Entry';
 import { RegisterEntry } from '../types/interfaces/Entry';
 
 export default class EntryModel {
+  getAll = async () => {
+    const data = await Entry.findAll();
+
+    return data;
+  };
+
   createEntry = async (data: RegisterEntry) => {
     const creating = await Entry.create({
       data,
