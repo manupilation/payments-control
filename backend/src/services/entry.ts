8350ca6c1748e73064f4b75269c4b3a4a68b3d81
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-09-10 15:08:27 -0300

    feat: method getByPk

--- backend/src/services/entry.ts
+++ backend/src/services/entry.ts
@@ -23,5 +23,11 @@ export default class EntryService {
     const data = await this.entryModel.getAllInDate(date);
 
     return data;
+  };
+
+  getByPk = async (id: number) => {
+    const data = await this.entryModel.getByPk(id);
+
+    return data;
   };  
 }
